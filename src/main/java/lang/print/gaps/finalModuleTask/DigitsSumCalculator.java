895291da9490bd8d63b9e33feeb9e88662cafdfe
nyuto <nuytonyka@gmail.com>
2023-01-30 16:55:15 +0200

    Solved

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum= 0;
+        while (number !=0){
+            int s = number%10;
+            sum = sum + s;
+            number/=10;
+        }
+        System.out.println(sum);
     }
 }
